3632b1c9f8d7886d65b34626b15294bb2eb50d2e
tanjil <tanjil14@cse.pstu.ac.bd>
2021-10-19 07:28:46 +0600

    add hooks and context

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,2 +1,37 @@
-import { GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import initializeAuthentication from "../Pages/Login/Firebase/Firebase.init";
 
+initializeAuthentication();
+
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
+  const signInUsingGoogle = () => {
+    const googleProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleProvider).then((result) => {
+      setUser(result.user);
+    });
+  };
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+    return () => unsubscribed;
+  }, [auth]);
+  return {
+    user,
+    signInUsingGoogle,
+  };
+};
+export default useFirebase;
